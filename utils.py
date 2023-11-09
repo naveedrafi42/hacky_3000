b58a3981e82bc52e84fdc2d94d53067245f291e7
Piaternev Ivan <31762927+pyaternev57@users.noreply.github.com>
2023-11-09 10:44:36 +0000

    Fix TypeError by converting input to string in extract_first_name

--- utils.py
+++ utils.py
@@ -1,16 +1,14 @@
 def extract_first_name(full_name):
+    # Ensure the full_name is a string
+    if not isinstance(full_name, str):
+        full_name = str(full_name)
     # Split the full name into words using whitespace as the separator
     name_parts = full_name.split()
-
     # Check if there are any words in the full name
     if len(name_parts) > 0:
         # The first word is the first name
         first_name = name_parts[0]
         return first_name
     else:
-        # If there are no words, return an empty string
+        # If there are no words return an empty string
         return ""
-
-
-def another_function(notrelated):
-    return
