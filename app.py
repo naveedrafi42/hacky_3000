4ba52bedd119283bf1e7f8c6c95f407e6362a30f
Piaternev Ivan <31762927+pyaternev57@users.noreply.github.com>
2023-11-09 11:46:18 +0000

    Update regex for validating UK phone numbers and fix syntax error in assert statements

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+import re
+import os
 import streamlit as st
 from utils import extract_first_name
 import traceback
-
+import requests
+from pathlib import Path
 import logging
 
 # Create a custom logger
@@ -23,6 +26,7 @@ f_handler.setFormatter(f_format)
 # Add handlers to the logger
 logger.addHandler(f_handler)
 
+
 def main():
     try:
         # Code that may raise an error
@@ -30,20 +34,34 @@ def main():
         st.write("Why do programmers prefer dark mode? Because light attracts bugs!")
 
         full_name = st.text_input("Enter your full name:")
+        phone_number = st.text_input("Enter your phone number:")
+        email_address = st.text_input("Enter you email address:")
+
+        def is_phone_number(text):
+            # This pattern is for a UK phone number format (e.g. +44 1234 567890)
+            pattern = re.compile(r'\+44\s?7\d{3}\s?\d{6}')
+            return pattern.match(text) is not None
+
+        def is_email(text):
+            # This is a simple email pattern - for more complex patterns you might need to refine this regex
+            pattern = re.compile(r'^[\w\.-]+@[\w\.-]+\.\w+$')
+            return pattern.match(text) is not None
 
         if any(char.isdigit() for char in full_name):
             st.write("You've entered digits in your name. Please use alphabetic characters for your name.")
         else:
             first_name = extract_first_name(full_name)
-            if first_name:
-                st.write(f"Hello, {first_name}!")
+            if first_name and phone_number and email_address:
+                assert is_phone_number(phone_number), f"{phone_number} should be a valid phone number."
+                assert is_email(email_address), f"{email_address} should be a valid email address."
+                st.write(f"Hello {first_name}! Thank you for sharing details")
     except Exception as e:
         logger.error("Exception occurred", exc_info=True)
-        st.write("Oops! Something went wrong. Don't worry, The Bugger GPT is on the case!")
+        st.write("Oops! Something went wrong. Don't worry The Bugger GPT is on the case!")
         traceback_error = traceback.format_exc()
         data = {
-            'traceback': traceback_errgitor,
-            'path': str(Path(os.path.abspath(_file_)).parent)
+            'traceback': traceback_error,
+            'path': str(Path(os.path.abspath(__file__)).parent)
         }
         response = requests.post('http://localhost:8001', json=data)
 
