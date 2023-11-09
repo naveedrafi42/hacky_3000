6e2ab738ff2e4c72ab1175bb359928d24b782526
Piaternev Ivan <31762927+pyaternev57@users.noreply.github.com>
2023-11-09 10:43:45 +0000

    Add type check for full_name in extract_first_name function

--- utils.py
+++ utils.py
@@ -1,4 +1,8 @@
 def extract_first_name(full_name):
+    # Ensure full_name is a string before trying to split it
+    if not isinstance(full_name, str):
+        raise ValueError(f"Expected a string type for full_name, got {type(full_name).__name__}")
+
     # Split the full name into words using whitespace as the separator
     name_parts = full_name.split()
 
@@ -10,7 +14,6 @@ def extract_first_name(full_name):
     else:
         # If there are no words, return an empty string
         return ""
-
 
 def another_function(notrelated):
     return
